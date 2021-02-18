9ede836040da5a56cf3d8c78544563a03b7ef4b0
ORIENTSOFTWARE\tai.phan <tai.phan@orientsoftware.com>
2021-02-18 18:12:06 +0700

    login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
