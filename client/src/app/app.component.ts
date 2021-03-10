2e026dba061db05949c46a27e6e8814e0d253e61
LAPTOP-5TV6RSLH\ASUS <tai.phan@orientsoftware.com>
2021-03-10 23:46:05 +0700

    create new client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { User } from './_models/user';
-import { AccountService } from './_Services/account.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Dating app';
-  users: any;
-
-  constructor(private accounService: AccountService) {}
-  
-  ngOnInit(): void {
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const user: User = JSON.parse(localStorage.getItem('user') || '{}');
-    this.accounService.setCurrentUser(user);
-  }
+export class AppComponent {
+  title = 'client';
 }
